0096070a77da2dbc24116e33f46b9446c3bedb60
xarthur <xliotx@gmail.com>
2021-07-24 23:01:08 +0200

    init

---
+++ igl_RhinoCommon/Utils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using Rhino.Geometry;
+
+namespace IGLRhinoCommon
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Sums two numbers
+        /// </summary>
+        public static double Sum(double a, double b)
+        {
+            return CppIGL.Add(a, b);
+        }
+        public static List<List<int>> getAdjacencyLst(ref Mesh rhinoMesh)
+        {
+            if (rhinoMesh == null) throw new ArgumentNullException(nameof(rhinoMesh));
+
+            // initialize the pointer and pass data
+            int nF = rhinoMesh.Faces.Count;
+            int numEle = 3 * rhinoMesh.Vertices.Count * 20;
+
+            // copy data into the IntPtr
+            //float[] V = rhino_mesh.Vertices.ToFloatArray();
+            int[] F = rhinoMesh.Faces.ToIntArray(true);
+            IntPtr meshF = Marshal.AllocHGlobal(Marshal.SizeOf(F[0]) * F.Length);
+            Marshal.Copy(F, 0, meshF, F.Length);
+
+            // assume each vert has most 10 neighbours
+            IntPtr adjLstFromCpp = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(int)) * numEle);
+
+            // call the c++ func
+            int sz;
+            CppIGL.igl_adjacency_list(meshF, nF, adjLstFromCpp, out sz);
+
+            int[] processedAdjLst = new int[numEle];
+            Marshal.Copy(adjLstFromCpp, processedAdjLst, 0, numEle);
+
+            // free memory
+            Marshal.FreeHGlobal(meshF);
+            Marshal.FreeHGlobal(adjLstFromCpp);
+
+            List<List<int>> adjLst = new List<List<int>>();
+            int cnt = 0;
+            while (cnt < sz)
+            {
+                int num = processedAdjLst[cnt];
+                cnt++;
+
+                List<int> transferLst = new List<int>();
+                for (int i = 0; i < num; i++)
+                {
+                    transferLst.Add(processedAdjLst[cnt++]);
+                }
+                adjLst.Add(transferLst);
+            }
+
+            // compute from cpp side.
+            return adjLst;
+        }
+    }
+}
